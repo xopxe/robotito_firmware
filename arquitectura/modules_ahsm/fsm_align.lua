ae6fe13d7725896fa90ad2e86dc13654407614f8
gamorin <guillermoamorin@gmail.com>
2018-10-18 16:30:02 -0300

    fsm update

--- arquitectura/modules_ahsm/fsm_align.lua
+++ arquitectura/modules_ahsm/fsm_align.lua
@@ -11,7 +11,7 @@ rfsm_timeevent.set_gettime_hook(function()
 end)
 --]]
 
-local ROTATION_SPEED = 0.008
+local ROTATION_SPEED = 0.59
 local w_rotate = 0
 
 local set_w = function(w)
@@ -29,10 +29,14 @@ local TIMEOUT_INC = 0.25
 local TIMEOUT_INIT = 0.25
 
 
-local last_min = 9000
+
 local DELTA = 5
 
-local TIMEOUT_SEARCH_60 = 3
+local TIMEOUT_SEARCH_60 = 2.0
+local min_init = 9000
+local last_min = min_init
+local measures = {min_init,min_init,min_init,min_init,min_init, min_init}
+
 
 local event_mt = {__tostring =function(v) return v.name end}
 local e_no_object = {name="e_no_object"}
@@ -60,24 +64,31 @@ local t_izq_no_to_stop
 local t_der_no_to_stop
 local t_buscar_min_to_ajuste
 local t_ajuste_to_going
---[[
-
-
-local t_izq_no_to_buscar_min
-local t_der_no_to_buscar_min
+local t_init_to_search
+local t_end_debug
 
 
 
+local init_s = ahsm.state{}
 
---]]
+local search_farest_object_s = ahsm.state{
+  entry=function() set_w(ROTATION_SPEED) end,
+  doo=function()
+    for i = 1,6 do
+      if (robot.background.actual_dist[i] < measures[i]) then
+        measures[i] = robot.background.actual_dist[i]
+      end
+    end
+  end,
+  exit=function()t_izq_no_to_der.timeout = TIMEOUT_INIT; set_w(0.0); print(table.unpack(measures))end,
+}
 
-
-local init_s = ahsm.state{entry=function() t_izq_no_to_der.timeout = TIMEOUT_INIT; end,}
 local izq_no_obj_s = ahsm.state{entry=function() set_w(ROTATION_SPEED) end,}
+
 local der_no_obj_s = ahsm.state{entry=invert_w,}
 
 local izq_to_der_s = ahsm.state{doo=function()
-    if (robot.background.dist_align > 0) then
+    if (robot.background.actual_dist[robot.background.id_align] > 0) then
       robot.fsm.send_event(e_to_buscar)
     else
       robot.fsm.send_event(e_continue_panning)
@@ -86,7 +97,7 @@ local izq_to_der_s = ahsm.state{doo=function()
   end}
 
 local der_to_izq_s = ahsm.state{doo=function()
-    if (robot.background.dist_align > 0) then
+    if (robot.background.actual_dist[robot.background.id_align] > 0) then
       robot.fsm.send_event(e_to_buscar)
     else
       robot.fsm.send_event(e_continue_panning)
@@ -97,8 +108,8 @@ local der_to_izq_s = ahsm.state{doo=function()
 local buscar_min_s = ahsm.state{
   entry = function() last_min = 9000 end,
   doo=function()
-    if ((robot.background.dist_align ~= 0) and (robot.background.dist_align < last_min)) then
-      last_min = robot.background.dist_align
+    if ((robot.background.actual_dist[robot.background.id_align] ~= 0) and (robot.background.actual_dist[robot.background.id_align] < last_min)) then
+      last_min = robot.background.actual_dist[robot.background.id_align]
     end
     return true
   end
@@ -109,8 +120,8 @@ local stop_s = ahsm.state{entry=function()
   end,}
 
 local ajuste_s = ahsm.state{entry=invert_w,
-    doo=function()
-    if ((robot.background.dist_align <= last_min + DELTA) and (robot.background.dist_align >= DELTA)) then
+  doo=function()
+    if ((robot.background.actual_dist[robot.background.id_align] <= last_min + DELTA) and (robot.background.actual_dist[robot.background.id_align] >= DELTA)) then
       robot.fsm.send_event(e_min_found)
       set_w(0,0)
     end
@@ -129,11 +140,23 @@ local going_s = ahsm.state{entry=function()
   end,}
 
 
+t_init_to_search = ahsm.transition{
+  src= init_s,
+  tgt= search_farest_object_s,
+  events = {init_s.EV_DONE}
+}
+
+t_end_debug = ahsm.transition{
+  src= search_farest_object_s,
+  tgt= going_s,
+  timeout = TIMEOUT_SEARCH_60,
+}
+
 
 t_init_to_izq_no_obj = ahsm.transition{
   src= init_s,
   tgt= izq_no_obj_s,
-  events={ e_no_object },
+  events={ "lalala"}, --e_no_object },
 }
 
 t_izq_no_to_der = ahsm.transition{
@@ -211,6 +234,7 @@ fsm = ahsm.state {
     EV_END_OBJECT = e_end_object},
   states = {  init = init_s,
     izq_no_obj = izq_no_obj_s,
+    search_farest_object = search_farest_object_s,
     der_no_obj = der_no_obj_s,
     izq_to_der = izq_to_der_s,
     der_to_izq = der_to_izq_s,
@@ -229,7 +253,9 @@ fsm = ahsm.state {
     t_izq_no_to_stop,
     t_der_no_to_stop,
     t_buscar_min_to_ajuste,
-    t_ajuste_to_going
+    t_ajuste_to_going,
+    t_init_to_search,
+    t_end_debug,
     --[[
     
     t_der_no_to_buscar_min,
