a1cc0e94c835a2ec828aa365322ce603b9abd4ff
Jorge Visca <jvisca@fing.edu.uy>
2018-11-09 10:45:47 -0300

    precompute #cbs

--- arquitectura/work/cb_list.lua
+++ arquitectura/work/cb_list.lua
@@ -1,17 +1,19 @@
 local M = {}
 
 M.get_list = function ()
-  local cbs = {}
+  local cbs = { n = 0 }
   cbs.append = function (cbs, cb)
     cbs[#cbs+1] = cb
+    cbs.n = #cbs
   end
   cbs.remove = function (cbs, cb)
     for i = 1, #cbs do 
       if cbs[i] == cb then table.remove(cbs, i) end
     end
+    cbs.n = #cbs
   end
   cbs.call = function (...)
-    for i = 1, #cbs do 
+    for i = 1, cbs.n do 
       cbs[i](...)
     end
   end
