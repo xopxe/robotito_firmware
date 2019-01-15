2435cd5e26643a308d9d489d239af53b563184bb
Jorge <xxopxe@gmail.com>
2019-01-15 12:50:08 -0300

    display reading on leds

--- test/modules_tests/test_laser_ring.lua
+++ test/modules_tests/test_laser_ring.lua
@@ -1,11 +1,18 @@
 --- Test laser ring.
 
-local TEST_SEC = 10 -- test for 10 seconds
+local TEST_SEC = 60 -- test for 60 seconds
 
 local laser=require('laser_ring')
+local ledr=require('led_ring')
 
 local dump_ranges = function(d1, d2, d3, d4, d5, d6)
   print ('dist (mm):', d1, d2, d3, d4, d5, d6)
+  local d = laser.norm_d
+  for i=1, 6 do
+    local v = 100-d[i]
+    ledr.set_segment(i, v, v, v)
+  end
+  ledr.update()
 end
 
 -- pick one of the next two
@@ -23,3 +30,4 @@ tmr.sleepms(TEST_SEC*1000)
 print('Done monitoring ranges')
 laser.enable(false)
 
+ledr.clear()
