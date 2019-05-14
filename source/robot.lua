ce3b2290007f51f3a69108967705a7fa849c5cbf
gamorin <guillermoamorin@gmail.com>
2019-05-14 11:26:39 -0300

    adaptación a color y remoto simultaneo

--- source/robot.lua
+++ source/robot.lua
@@ -1,10 +1,28 @@
 --- Robot module.
 -- This is the main module for the robot. Contains all the services available.
--- All the services are already initalized using data stored in non-volatile 
+-- All the services are already initalized using data stored in non-volatile
 -- storage. Check each module's documentaion to see the used variables.
 -- @module robot
 -- @alias M
 local M = {}
+
+function implode(delimiter, list)
+  local len = #list
+  if len == 0 then
+    return ""
+  end
+  local string = list[1]
+  for i = 2, len do
+    string = string .. delimiter .. list[i]
+  end
+  return string
+end
+
+local LASER_CMD = 'laser'
+local COLOR_CMD = 'color'
+local DELIMITER = '*'
+
+M.hsm = nil
 
 --- Unique ID of the robot.
 -- Read from `nvs.read("robot","id")`, defaults to 0.
@@ -17,7 +35,7 @@ M.omni.enable()
 --- Downard facing @{color} sensor.
 M.color = require('color')
 
---- Downard facing @{proximity} sensor. 
+--- Downard facing @{proximity} sensor.
 M.floor = require('proximity')
 
 --- Distance sensor ring. This points to @{laser_ring}
@@ -30,5 +48,38 @@ M.led_ring = require'led_ring'
 M.wifi_net = require('wifi_net')
 M.wifi_net.init()
 
+
+local filter = nvs.read("laser", "filter", nil) or nil
+print('filter loading:', filter)
+
+local measure_cb = M.laser_ring.get_reading_cb()
+if (filter == "filter") then
+  measure_cb = M.laser_ring.get_filtering_cb()
+end
+
+
+--local laser_ring_publisher = function (d1,d2,d3,d4,d5,d6)
+--  measure_cb(d1,d2,d3,d4,d5,d6)
+--  local sens_str = nil
+--  if (filter == nil) then
+--    sens_str = LASER_CMD .. DELIMITER .. implode(DELIMITER, M.laser_ring.raw_d)
+--  else
+--    sens_str = LASER_CMD .. DELIMITER .. implode(DELIMITER, M.laser_ring.norm_d)
+--  end
+--  M.wifi_net.broadcast(sens_str)
+--end
+--
+--local color_cb = function(color_name, h, s, v)
+--  local color_str = COLOR_CMD .. DELIMITER .. color_name .. DELIMITER .. h .. DELIMITER .. s .. DELIMITER .. v
+--  M.wifi_net.broadcast(color_str)
+--end
+--
+--
+
+--M.laser_ring.cb.append(laser_ring_publisher)
+--M.color.color_cb.append(color_cb)
+
+--M.laser_ring.enable(true)
+--M.color.enable(true)
 
 return M
---
+++ source/states/onremoteoff.lua
@@ -0,0 +1,61 @@
+--- On-floor detecting state machine.
+-- This state machine is usefull for powering on and off the robot
+-- when placed on the floor or picked up. It uses the proximity sensor.
+-- To integrate, import t nd add behavior to the states.ON and states.OFF
+-- states. Also emits events.FLOOR and events.NOTFLOOR events.
+-- For an example of a state mechine using this, see @{colorway.lua}
+-- @hsm onoff.lua
+
+local ahsm = require 'ahsm'
+
+-- events for proximity sensor
+local e_floor = { _name="FLOOR" }
+local e_not_floor = { _name="NOTFLOOR" }
+
+
+-- On/Off states
+local s_off = ahsm.state {
+  entry = function ()
+    robot.led_ring.clear()
+    robot.omni.enable(false)
+  end,
+}
+
+local s_on =  require( "states.remotecontrol" )
+
+-- On/Off transitions on proximity events
+local t_on = ahsm.transition {
+  src = s_off, tgt = s_on,
+  events = { e_floor },
+}
+local t_off = ahsm.transition {
+  src = s_on, tgt = s_off,
+  events = { e_not_floor },
+}
+
+-- callback for proximity sensor, emits events for state machine
+local floor_event = function( v )
+  if v then
+    robot.hsm.queue_event(e_floor)
+  else
+    robot.hsm.queue_event(e_not_floor)
+  end
+end
+
+-- root state, embeds On/Off machine and initialization code
+local hsm = ahsm.state {
+  events =  { FLOOR=e_floor, NOTFLOOR=e_not_floor },
+  states = { OFF=s_off, ON=s_on },
+  transitions = { switchon=t_on, switchoff=t_off },
+  initial = s_off,
+  entry = function()
+    robot.floor.cb.append(floor_event)
+    robot.floor.enable(true)
+  end,
+  exit = function()
+    robot.floor.enable(false)
+    robot.floor.cb.remove(floor_event)
+  end,
+}
+
+return hsm
