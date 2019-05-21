4f7a8aeba2e599ea2d6d58922ba95d7c7120eb71
Jorge <xxopxe@gmail.com>
2019-05-21 17:35:54 -0300

    use filtered readings

--- source/robot.lua
+++ source/robot.lua
@@ -76,7 +76,8 @@ end
 --
 --
 
-M.laser_ring.cb.append(M.laser_ring.get_reading_cb())
+M.laser_ring.cb.append(M.laser_ring.get_filtering_cb())
+--M.laser_ring.cb.append(M.laser_ring.get_reading_cb())
 --M.color.color_cb.append(color_cb)
 
 M.laser_ring.enable(true)
