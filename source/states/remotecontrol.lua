da82ee37ec70d97b564e73c3158cc95b85f0553e
Jorge Visca <jvisca@fing.edu.uy>
2019-12-18 11:52:22 -0300

    initial version

--- source/states/remotecontrol.lua
+++ source/states/remotecontrol.lua
@@ -1,6 +1,8 @@
 local ahsm = require 'ahsm'
 
 local VEL_CMD = 'speed'
+local NVS_WRITE = 'nvswrite'
+local DO_STEP = 'step'
 
 local e_msg = { _name="WIFI_MESSAGE", cmd = nil,}
 local e_fin = { _name="FINCONTROL", }
@@ -20,11 +22,11 @@ local s_remote_control = ahsm.state {
 }
 
 local function split(s, delimiter)
-    local result = {};
-    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
-        table.insert(result, match)
-    end
-    return result
+  local result = {};
+  for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+    table.insert(result, match)
+  end
+  return result
 end
 
 local t_command = ahsm.transition {
@@ -36,13 +38,13 @@ local t_command = ahsm.transition {
       local data = ev.data
       if data[1] == VEL_CMD then
         if #data == 5 then
-            local xdot = data[2]
-            local ydot = data[3]
-            local w = data[4]
-            robot.omni.drive(xdot,ydot,w)
+          local xdot = data[2]
+          local ydot = data[3]
+          local w = data[4]
+          robot.omni.drive(xdot,ydot,w)
         end
-      end -- Mas comandos con else if
-        if data[1] == 'nvswrite' then
+      elseif data[1] == NVS_WRITE then
+        if #data == 5 then
           local namespace = data[2]
           local variable = data[3]
           local value = data[4]
@@ -51,30 +53,44 @@ local t_command = ahsm.transition {
           if type=='nil' then value=nil end
           nvs.write(namespace, variable, value)
         end
+      elseif data[1] == DO_STEP then
+        if #data == 4 then
+          local dir = data[2]
+          local dt = data[3]
+          local v = data[4]
+          local xdot, ydot = 0, 0
+          if dir=='N' then ydot=v
+          elseif dir=='S' then ydot=-v
+          elseif dir=='E' then xdot=v
+          elseif dir=='W' then xdot=-v end
+          robot.omni.drive(xdot,ydot,0)
+          tmr.sleepms( math.floor(1000*dt) )
+          robot.omni.drive(0,0,0)
+        end
       else
-      robot.hsm.queue_event(e_fin)
+        robot.hsm.queue_event(e_fin)
+      end
     end
-  end
-}
+  }
 
-local event_message = function(data,ip,port)
-  data = split(data, '*')
-  e_msg.data = data
-  robot.hsm.queue_event(e_msg)
-end
+  local event_message = function(data,ip,port)
+    data = split(data, '*')
+    e_msg.data = data
+    robot.hsm.queue_event(e_msg)
+  end
 
 -- root state
-local remote = ahsm.state {
-  events =  { WIFIMESSAGE = e_msg, FINCONTROL = e_fin },
-  states = { REMOTECONTROL=s_remote_control},
-  transitions = { COMMAND=t_command},
-  initial = s_remote_control,
-  entry = function()
-    robot.wifi_net.cb.append(event_message)
-  end,
-  exit = function()
-    robot.wifi_net.cb.remove(event_message)
-  end,
-}
+  local remote = ahsm.state {
+    events =  { WIFIMESSAGE = e_msg, FINCONTROL = e_fin },
+    states = { REMOTECONTROL=s_remote_control},
+    transitions = { COMMAND=t_command},
+    initial = s_remote_control,
+    entry = function()
+      robot.wifi_net.cb.append(event_message)
+    end,
+    exit = function()
+      robot.wifi_net.cb.remove(event_message)
+    end,
+  }
 
-return remote
+  return remote
