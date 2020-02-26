cf20f9d474b021c52ce516623ecf723b9636e223
sllavayol <sllavayol@fing.edu.uy>
2020-02-26 18:12:55 -0300

    prolijo

--- source/states/remotecontrol.lua
+++ source/states/remotecontrol.lua
@@ -8,12 +8,14 @@ local NVS_WRITE = 'nvswrite'
 local DO_STEP = 'step'
 local DO_TURN = 'turn'
 local LIGHT_SWITCHER = 'switcher'
-local LIGHT_MODE = 'mode' --white/color
+local LIGHT_MODE = 'mode'
 
 local e_msg = { _name="WIFI_MESSAGE", cmd = nil,}
 local e_fin = { _name="FINCONTROL", }
 
---local offset_led = 2
+
+
+-----
 
 local id_local = -1
 local lights_on = false
@@ -66,98 +68,112 @@ local s_remote_control = ahsm.state {
   end,
 }
 
+
 local function split(s, delimiter)
-  local result = {};
-  for match in (s..delimiter):gmatch("(.-)"..delimiter) do
-    table.insert(result, match)
-  end
-  return result
+    local result = {};
+    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(result, match)
+    end
+    return result
 end
 
+
+
+-----
+
+
+
+
 local t_command = ahsm.transition {
-  src = s_remote_control, tgt = s_remote_control,
-  events = { e_msg },
-  timeout = 5.0,
-  effect = function (ev)
-    if (ev == e_msg) then
-      local data = ev.data
-      if data[1] == VEL_CMD then
-        if #data == 5 then
-          local xdot = data[2]
-          local ydot = data[3]
-          local w = data[4]
-          robot.omni.drive(xdot,ydot,w)
-        end
+	src = s_remote_control, tgt = s_remote_control,
+	events = { e_msg },
+	timeout = 5.0,
+	effect = function (ev)
+		if (ev == e_msg) then
+			local data = ev.data
 
-      elseif data[1] == NVS_WRITE then
-        if #data == 5 then
-          local namespace = data[2]
-          local variable = data[3]
-          local value = data[4]
-          local type = data[5]
-          if type=='number' then value=tonumber(value) end
-          if type=='nil' then value=nil end
-          nvs.write(namespace, variable, value)
-        end
+			if data[1] == VEL_CMD and #data == 5 then ----
 
-      elseif data[1] == DO_STEP then
-        if #data == 5 then
-          local coord = data[2]
-          local dt = math.min(data[3],2)
-          local v = data[4]
-          local id = data[5]
-		print('dt:',dt)
+				local xdot = data[2]
+				local ydot = data[3]
+				local w = data[4]
+				robot.omni.drive(xdot,ydot,w)
 
-          local t = step_info[coord]
+			elseif data[1] == NVS_WRITE and #data == 5 then ----
 
-          if id_local ~= id then --havent read this message
-            id_local = id
-            if lights_on then
-		if white_on then
-			ledr.set_arc(t.led -2, 5, 20, 20, 20, true)
-		else
-			ledr.set_arc(t.led -2, 5, t.r, t.g, t.b, true)
+				local namespace = data[2]
+				local variable = data[3]
+				local value = data[4]
+				local type = data[5]
+				if type=='number' then
+					value=tonumber(value)
+				end
+				if type=='nil' then
+					value=nil
+				end
+				nvs.write(namespace, variable, value)
+
+			elseif data[1] == DO_STEP and #data == 5 then ----
+
+				local coord = data[2]
+				local dt = data[3]
+				local v = data[4]
+				local id = data[5]
+
+				local t = step_info[coord]
+
+				if id_local ~= id then --havent read this message
+					id_local = id
+					if lights_on then
+						if white_on then
+							ledr.set_arc(t.led -2, 5, 20, 20, 20, true)
+						else
+							ledr.set_arc(t.led -2, 5, t.r, t.g, t.b, true)
+						end
+					end
+					robot.omni.drive(v*t.x, v*t.y, 0)
+					tmr.sleepms(math.floor(1000*dt))
+					robot.omni.drive(0,0,0)
+				end
+
+			elseif data[1] == DO_TURN and #data == 5 then ----
+
+				local dir = data[2]
+				local dt = data[3]
+				local v = data[4]*5
+				local id = data[5]
+
+				if id_local ~= id then
+					id_local = id
+					local w = v; -- I asume that we turn left
+					if dir == 'R' then 
+						w = -v
+					end
+					robot.omni.drive(0,0,w)
+					tmr.sleepms(math.floor(1000*dt))
+					robot.omni.drive(0,0,0)
+				end
+
+			elseif data[1] == LIGHT_SWITCHER and #data == 2 then ----
+
+				lights_on = (data[2] == 'on')
+
+			elseif data[1] == LIGHT_MODE and #data == 2 then ----
+
+				white_on = (data[2] == 'white')
+
+			end ----
+
+		else --(ev == e_msg)
+			robot.hsm.queue_event(e_fin)
 		end
-            end
-            robot.omni.drive(v*t.x, v*t.y, 0)
-            --tmr.sleepms(math.floor(1000))
-            tmr.sleepms(math.floor(1000*dt))
-            robot.omni.drive(0,0,0)
-          end
-        end
-
-      elseif data[1] == DO_TURN then
-        if #data == 5 then
-          local dir = data[2]
-          local dt = data[3]
-          local v = data[4]*5
-          local id = data[5]
-
-          if id_local ~= id then
-            id_local = id
-            local w = v; -- I asume that we turn left
-            if dir == 'R' then w = -v end
-            robot.omni.drive(0,0,w)
-            tmr.sleepms(math.floor(1000*dt))
-            robot.omni.drive(0,0,0)
-          end
-        end
-
-      elseif data[1] == LIGHT_SWITCHER then
-        if #data == 2 then
-          lights_on = (data[2] == 'on')         
-        end
-
-      elseif data[1] == LIGHT_MODE then
-        if #data == 2 then
-          white_on = (data[2] == 'white')
-        end
-      end
-    else
-      robot.hsm.queue_event(e_fin)
-    end
-  end
+	end
 }
+
+
+
+-----
+
 
 local event_message = function(data,ip,port)
   data = split(data, '*')
